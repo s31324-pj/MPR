53ad85fd5916fa94901ae31f4d5fe3ddc548d7b4
s31324-pj <146528080+s31324-pj@users.noreply.github.com>
2025-01-25 14:39:47 +0100

    Dodanie plików projektu
    Nie udało mi się rozwiązać błędu "java: package org.junit.jupiter.api does not exist".

---
+++ src/main/java/com/pjatk/s31324Bank/S31324BankApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.s31324Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S31324BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S31324BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s31324Bank/S31324BankApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.pjatk.s31324Bank;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S31324BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
